06f30e9dfc240127937538a86df8a5b2eedab382
beebdev <beebdev@gmail.com>
2021-07-29 03:59:32 +1000

    Decode

--- rssalut.py
+++ rssalut.py
@@ -1,7 +1,180 @@
 #! /usr/bin/python3
-import csv
 
-with open("data/gesture_0/out1.csv") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        print(row)
+import csv
+import matplotlib.pyplot as plt
+from numpy.core.numeric import Inf
+from numpy.lib.function_base import median
+import pywt
+from fastdtw import fastdtw
+import mlpy
+from scipy.spatial.distance import euclidean
+from scipy import stats
+import sklearn.preprocessing as skprep
+import numpy as np
+
+res_factor = 5
+smooth_factor = 15
+trigger_sensitivity = 7
+gestures = ["leg_v", "leg_h", "leg_s", "hand_s"]
+
+reference = {}
+time = {}
+
+
+def mad(arr):
+    """ Median Absolute Deviation: a "Robust" version of standard deviation.
+        Indices variability of the sample.
+        https://en.wikipedia.org/wiki/Median_absolute_deviation
+    """
+    arr = np.ma.array(arr).compressed()
+    med = np.median(arr)
+    return np.median(np.abs(arr - med))
+
+
+def extract_signal(filename):
+    with open(filename) as csv_file:
+        headers = []
+        packets = {}
+        reader = csv.reader(csv_file, delimiter=",")
+        for c, row in enumerate(reader):
+            if c == 0:
+                for field in row:
+                    packets[field] = []
+                    headers.append(field)
+            else:
+                for nf, value in enumerate(row):
+                    packets[headers[nf]].append(value)
+    # Timeline and reference
+    timeline = [float(i) for i in packets["Time"]]
+    dBm = [int(i[:-3]) for i in packets["Signal strength (dBm)"]]
+
+    # get 1/3 of data
+    timeline = timeline[::res_factor]
+    dBm = dBm[::res_factor]
+    dBm = skprep.scale(
+            dBm, axis=0, with_mean=True, with_std=True, copy=True
+        )
+
+    # Denoising
+    w = pywt.Wavelet('haar')
+    w_coef = pywt.wavedec(dBm, w)
+    # sigma = mad(w_coef[-3])
+    sigma = 0.8
+    th = sigma * np.sqrt(2*np.log(len(dBm)))
+    w_coef[1:] = (pywt.threshold(i, value=th, mode='soft')
+                  for i in w_coef[1:])
+    denoised_dBm = pywt.waverec(w_coef, 'haar')
+
+    #smooth
+    box = np.ones(smooth_factor)/smooth_factor
+    denoised_dBm = np.convolve(denoised_dBm, box, mode='same')
+    return denoised_dBm, timeline
+
+
+def extract_ref(filename, gesture):
+    ''' Extracts a single file of reference sequence for a specifc gesture
+    '''
+    denoised_dBm, timeline = extract_signal(filename)
+    sublen = int(len(denoised_dBm) / 3)
+    for i in range(0, 3):
+        reference[gesture].append(denoised_dBm[sublen*i:sublen*(i+1)])
+        time[gesture].append(timeline[sublen*i:sublen*(i+1)])
+
+
+def extract_references():
+    # list for reference signals sequences
+    for g in gestures:
+        reference[g] = []
+        time[g] = []
+    extract_ref("data/leg_vertical/leg_v_01.csv", "leg_v")
+    extract_ref("data/leg_vertical/leg_v_02.csv", "leg_v")
+    extract_ref("data/leg_horizontal/leg_h_01.csv", "leg_h")
+    extract_ref("data/leg_horizontal/leg_h_02.csv", "leg_h")
+    extract_ref("data/leg_step/leg_s_01.csv", "leg_s")
+    extract_ref("data/leg_step/leg_s_02.csv", "leg_s")
+    # extract_ref("data/leg_inf/leg_i_01.csv", "leg_i")
+    # extract_ref("data/leg_inf/leg_i_02.csv", "leg_i")
+    # extract_ref("data/leg_inf/leg_i_03.csv", "leg_i")
+    extract_ref("data/hand_snatch/hand_s_01.csv", "hand_s")
+
+def RSS_decode(signal):
+    encode = "n"
+    s_min = min(signal)
+    s_max = max(signal)
+    trigger = (s_max - s_min) / trigger_sensitivity
+    prev = signal[0]
+    stopwatch = 0
+    sw_start = False
+    i = 10
+    while i < len(signal) and stopwatch < 250:
+    # for i in range(10, len(signal), 10):
+        if sw_start == True:
+            stopwatch += 1
+        curr = signal[i]
+        diff = curr - prev
+        if diff > trigger:
+            sw_start = True
+            c_state = "r"
+        elif diff < -trigger:
+            sw_start = True
+            c_state = "f"
+        else:
+            c_state = "n"
+        prev = curr
+        i += 10
+        if encode[-1] == c_state:
+            continue
+        else:
+            encode += c_state
+    return encode
+
+
+def RSS_dtw(x, y):
+    dist, _ = fastdtw(x, y, dist=euclidean)
+    return dist
+
+def classify_test(filename):
+    denoised_dBm, _ = extract_signal(filename)
+    test_cases = []
+    sublen = int(len(denoised_dBm) / 3)
+    for i in range(0, 3):
+        test_cases.append(denoised_dBm[sublen*i:sublen*(i+1)])
+
+
+    for case in test_cases:
+        best = Inf
+        tag = None
+        num = None
+        for g in gestures:
+            # print(g)
+            sum = 0
+            for c, subref in enumerate(reference[g]):
+                # print(subref)
+                # print(case)
+                curr = RSS_dtw(case, subref)
+                print(g, c, curr)
+                sum += curr
+            avg = sum / len(reference[g])
+            if avg < best:
+                best = avg
+                tag = g
+        print("choose: ", tag, best)
+        # break
+
+def show_signal(gesture):
+    plt.figure()
+    print(gesture, len(reference[gesture]))
+    for i in range(0, len(reference[gesture])):
+        print(RSS_decode(reference[gesture][i]))
+        my_min = min(len(time[gesture][i]), len(reference[gesture][i]))
+        plt.subplot(len(reference[gesture]), 1, i+1)
+        plt.plot(time[gesture][i][:my_min], reference[gesture][i][:my_min])
+        plt.xlabel('Time (s)')
+    plt.savefig(gesture+".png")
+
+
+if __name__ == "__main__":
+    extract_references()
+    # classify_test("data/testset/vhs01.csv")
+    for g in gestures:
+        show_signal(g)
--- test.py
+++ test.py
@@ -1,7 +1,142 @@
 #! /usr/bin/python3
-
+import csv
 import pywt
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as skprep
 
-fam = pywt.families()
-for f in fam:
-    print(pywt.wavelist(f))
+def denoise_signal(signal):
+    w = pywt.Wavelet('haar')
+    w_coef = pywt.wavedec(signal, w)
+    # sigma = mad(w_coef[-3])
+    sigma = 0.8
+    th = sigma * np.sqrt(2*np.log(len(signal)))
+    w_coef[1:] = (pywt.threshold(i, value=th, mode='soft')
+                for i in w_coef[1:])
+    return pywt.waverec(w_coef, 'haar')
+
+def smoothen_signal(signal, factor):
+    box = np.ones(factor)/factor
+    return np.convolve(signal, box, mode='same')
+
+def RSS_decode(signal, trigger_factor):
+    encode = "n"
+    s_min = min(signal)
+    s_max = max(signal)
+    trigger = (s_max - s_min) / trigger_factor
+
+    prev = signal[0]
+    stopwatch = 0
+    sw_start = False
+    for i in range(10, len(signal), 10):
+        if sw_start:
+            stopwatch += 1
+        
+        curr = signal[i]
+        diff = curr - prev
+        prev = curr
+        if diff > trigger:
+            sw_start = True
+            state = "r"
+        elif diff < trigger:
+            sw_start = True
+            state = "f"
+        else:
+            state = "n"
+        # Check state change
+        if encode[-1] == state:
+            continue
+        else:
+            encode += state
+    return encode
+
+
+class RSSalut:
+    # factors
+    res_factor = 5
+    smooth_factor = 15
+
+    # data
+    gestures = ["leg_v", "leg_h", "leg_s", "hand_s"]
+    reference = {}
+    timeline = {}
+    encoded = {}
+
+    def __init__(self):
+        # Initialise reference data structs
+        for g in self.gestures:
+            # print(g)
+            self.reference[g] = []
+            self.timeline[g] = []
+            self.encoded[g] = []
+
+        # Extract reference
+        self.extract_references()
+
+        
+    def extract_signal(self, filename):
+        # Open csv data file
+        with open(filename) as csv_file:
+            headers = []
+            packets = {}
+            reader = csv.reader(csv_file, delimiter=",")
+            for c, row in enumerate(reader):
+                if c == 0:
+                    for field in row:
+                        packets[field] = []
+                        headers.append(field)
+                else:
+                    for nf, value in enumerate(row):
+                        packets[headers[nf]].append(value)
+
+        # Timeline and reference
+        timeline = [float(i) for i in packets["Time"]]
+        dBm = [int(i[:-3]) for i in packets["Signal strength (dBm)"]]
+        
+        # lower resolution
+        timeline = timeline[::self.res_factor]
+        dBm = dBm[::self.res_factor]
+        dBm = skprep.scale(
+                dBm, axis=0, with_mean=True, with_std=True, copy=True
+            )
+
+        # Process signal
+        dBm = denoise_signal(dBm)
+        dBm = smoothen_signal(dBm, self.smooth_factor)
+        return dBm, timeline
+
+    def extract_ref(self, filename, gesture):
+        dBm, timeline = self.extract_signal(filename)
+        sublen = int(len(dBm) / 3)
+        for i in range(0, 3):
+            self.reference[gesture].append(dBm[sublen*i:sublen*(i+1)])
+            self.timeline[gesture].append(timeline[sublen*i:sublen*(i+1)])
+
+    def extract_references(self):
+        self.extract_ref("data/leg_vertical/leg_v_01.csv", "leg_v")
+        self.extract_ref("data/leg_vertical/leg_v_02.csv", "leg_v")
+        self.extract_ref("data/leg_horizontal/leg_h_01.csv", "leg_h")
+        self.extract_ref("data/leg_horizontal/leg_h_02.csv", "leg_h")
+        self.extract_ref("data/leg_step/leg_s_01.csv", "leg_s")
+        self.extract_ref("data/leg_step/leg_s_02.csv", "leg_s")
+        # self.extract_ref("data/leg_inf/leg_i_01.csv", "leg_i")
+        # self.extract_ref("data/leg_inf/leg_i_02.csv", "leg_i")
+        # self.extract_ref("data/leg_inf/leg_i_03.csv", "leg_i")
+        self.extract_ref("data/hand_snatch/hand_s_01.csv", "hand_s")
+
+    def show_signal(self, gesture):
+        plt.figure()
+        for i in range(0, len(self.reference[gesture])):
+            my_min = min(len(self.timeline[gesture][i]), len(self.reference[gesture][i]))
+            plt.subplot(len(self.reference[gesture]), 1, i+1)
+            plt.plot(self.timeline[gesture][i][:my_min], self.reference[gesture][i][:my_min])
+            plt.xlabel('Time (s)')
+        plt.savefig(gesture+".png")
+
+    def show_all_signals(self):
+        for g in self.gestures:
+            self.show_signal(g)
+
+if __name__ == "__main__":
+    rssalut = RSSalut()
+    rssalut.show_all_signals()
